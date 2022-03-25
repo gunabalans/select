38386f108fe8a8a9792ff3fc8e033387e67ec469
gunabalans <gunabalans@yahoo.com>
2022-03-25 20:16:59 +0530

    Merge branch 'main' of https://github.com/gunabalans/select into main

---
+++ test.php
@@ -0,0 +1,4 @@
+A Technical architect also referred to as an IT Systems Architect, is a systems logistics specialist who creates, maintains, and implements IT systems for a growing business or IT firm
+
+https://www.simplilearn.com/technical-architect-article
+https://www.simplilearn.com/how-to-build-career-in-it-service-management-article
